adcc16c489667e3826a9c155b9fa6561bc8f7342
nguyendinhthong <nguyendinhthongit@gmail.com>
2020-03-31 22:04:45 +0700

    add more text to describe the program, and test with other sheets in new BOM ok

--- script_2.py
+++ script_2.py
@@ -1,39 +1,41 @@
 #https://stackoverflow.com/questions/51800122/using-openpyxl-to-find-rows-that-contain-cell-with-specific-value-python-3-6/51801421
 from openpyxl import Workbook
 import openpyxl
-print("Hello world")
-file = "./../thong_dmvt_lo1048/file_goc/EBOM_VEE-B01_Lan 11_Kien.xlsx"
-wb = openpyxl.load_workbook(file, read_only=True)
-#print(wb.sheetnames)
+print("SAP B1 auto tool for TSAN")
+excel_path_suffix = "./../thong_dmvt_lo1048/file_goc/"
+file = excel_path_suffix + "EBOM_VEE-B01_Lan 11_Kien.xlsx"
+new_bom_wb = openpyxl.load_workbook(file, read_only=True)
+#print(new_bom_wb.sheetnames)
 # Select a specific sheet to work
-ws = wb["PL6_BOM_machchinh"]
-ma_nsx_list = []
+new_bom_sheet = "PL13_BOM_Machsacngoai"
+ws = new_bom_wb[new_bom_sheet]
+component_code_list = []
 # iterate every row in column 
 i = 1
-wb_first_row = 14
-wb_last_row = 147
-for row in ws.iter_rows("I"):
+# limit to the rows where Ma NSX are available
+wb_first_row = 9
+wb_last_row = 37
+for row in ws.iter_rows("F"):
     for cell in row:
-        # limit to the rows where Ma NSX are available
         # note that some cases we must manually link in order to complete an Excel sheet. 
         if i>=wb_first_row and i<=wb_last_row:
-            ma_nsx_list.append([i,cell.value])
+            component_code_list.append([i,cell.value])
             #print(cell.value)
         # increase row index
         i = i+1
 print("There're total of " + str(wb_last_row-wb_first_row+1) + " component to be map the new SAP B1 component code")
 # print the list contain all ma nxs to map to target file along with its row index in previous file to Danh muc vat tu LINH KIEN.xlsx
-#print(ma_nsx_list)
-file1 = "./../thong_dmvt_lo1048/file_goc/Danh muc vat tu LINH KIEN.xlsx"
+#print(component_code_list)
+file1 = excel_path_suffix + "Danh muc vat tu LINH KIEN.xlsx"
 component_code_match_row_index = 1
 wb1 = openpyxl.load_workbook(file1, read_only=True)
 ws1 = wb1.active
 print(wb1.sheetnames) 
-print(len(ma_nsx_list))
-# search each item in ma_nsx_list in every row of column C (Ky ma hieu) in `Danh muc vat tu LINH KIEN.xlsx`
+print(len(component_code_list))
+# search each item in component_code_list in every row of column C (Ky ma hieu) in `Danh muc vat tu LINH KIEN.xlsx`
 component_code_cnt = 0
 none1_cnt = 0
-for item in ma_nsx_list:
+for item in component_code_list:
     #print(item[1])
     for row in ws1.iter_rows("C"):
         for cell in row:
@@ -56,29 +58,25 @@ for item in ma_nsx_list:
         # reset the counter for next loop    
     component_code_match_row_index = 1
 # STT in Excel 1, Component Code, STT in Excel 2, Ma vat tu cu
-#print(ma_nsx_list)
-print("Number of items matched by component code: " + str(component_code_cnt-none1_cnt)+" / "+str(wb_last_row-wb_first_row+1))
+#print(component_code_list)
+print("Number of items matched by component code: " + str(component_code_cnt-none1_cnt) + " / " + str(wb_last_row-wb_first_row+1))
 nsx_code_match_row_index = 1
-file2 = "./../thong_dmvt_lo1048/file_goc/List of Items 31-03-2020.xlsx"
+file2 = excel_path_suffix + "List of Items 31-03-2020.xlsx"
 wb2 = openpyxl.load_workbook(file2, read_only=True)
 ws2 = wb2.active
 print(wb2.sheetnames)
 
-for item in ma_nsx_list:
+for item in component_code_list:
     for row in ws2.iter_rows("F"):
         for cell in row:
             # if we detect any matching
-            #print(cell.value)
-            #print(item[3])
-            #print("row_index" + str(nsx_code_match_row_index))
             if cell.value == item[3]:
-                #print("hi")
                 print(item[0])
-                print(item[1])
+                print("Component code (Ma NXS1): " + str(item[1]))
                 print(item[2])
-                print(item[3])
+                print("Foreign Name (Ma cu): " + str(item[3]))
                 #print(nsx_code_match_row_index)
-                print(ws2.cell(None, nsx_code_match_row_index, 2).value)
+                print("Item No. (Ma moi): " + str(ws2.cell(None, nsx_code_match_row_index, 2).value))
                 #item.append(component_code_match_row_index)
                 if ws2.cell(None, nsx_code_match_row_index, 2).value == None:
                     print("None value detected, we won't use this for the next code")
@@ -88,4 +86,4 @@ for item in ma_nsx_list:
         # reset the counter for next loop    
     nsx_code_match_row_index = 1
 
-print(ma_nsx_list)
+print(component_code_list)
