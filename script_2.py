cf63145680cd8f4afef1edf12fbe3f6feb58af00
nguyendinhthong <nguyendinhthongit@gmail.com>
2020-04-01 08:15:02 +0700

    problem in first excel sheet, dont know why

--- script_2.py
+++ script_2.py
@@ -1,4 +1,8 @@
 #https://stackoverflow.com/questions/51800122/using-openpyxl-to-find-rows-that-contain-cell-with-specific-value-python-3-6/51801421
+# component_code_list = [STT in Excel 1, Component Code, STT in Excel 2, Ma vat tu cu]
+# excel sheet name
+# wb range for first sheet
+#
 from openpyxl import Workbook
 import openpyxl
 print("SAP B1 auto tool for TSAN")
@@ -7,25 +11,23 @@ file = excel_path_suffix + "EBOM_VEE-B01_Lan 11_Kien.xlsx"
 new_bom_wb = openpyxl.load_workbook(file, read_only=True)
 #print(new_bom_wb.sheetnames)
 # Select a specific sheet to work
-new_bom_sheet = "PL13_BOM_Machsacngoai"
+new_bom_sheet = "PL6_BOM_machchinh"
 ws = new_bom_wb[new_bom_sheet]
 component_code_list = []
 # iterate every row in column 
 i = 1
-# limit to the rows where Ma NSX are available
+# limit to the rows where Ma NSX1 are available
 wb_first_row = 9
-wb_last_row = 37
-for row in ws.iter_rows("F"):
+wb_last_row = 147
+for row in ws.iter_rows("I"):
     for cell in row:
         # note that some cases we must manually link in order to complete an Excel sheet. 
         if i>=wb_first_row and i<=wb_last_row:
             component_code_list.append([i,cell.value])
-            #print(cell.value)
-        # increase row index
         i = i+1
 print("There're total of " + str(wb_last_row-wb_first_row+1) + " component to be map the new SAP B1 component code")
 # print the list contain all ma nxs to map to target file along with its row index in previous file to Danh muc vat tu LINH KIEN.xlsx
-#print(component_code_list)
+
 file1 = excel_path_suffix + "Danh muc vat tu LINH KIEN.xlsx"
 component_code_match_row_index = 1
 wb1 = openpyxl.load_workbook(file1, read_only=True)
@@ -41,7 +43,6 @@ for item in component_code_list:
         for cell in row:
             # if we detect any matching by component code
             if cell.value == item[1]:
-                #print("hi")
                 component_code_cnt = component_code_cnt+1
                 print(component_code_cnt)
                 print(item[0])
@@ -57,7 +58,7 @@ for item in component_code_list:
             component_code_match_row_index = component_code_match_row_index+1
         # reset the counter for next loop    
     component_code_match_row_index = 1
-# STT in Excel 1, Component Code, STT in Excel 2, Ma vat tu cu
+
 #print(component_code_list)
 print("Number of items matched by component code: " + str(component_code_cnt-none1_cnt) + " / " + str(wb_last_row-wb_first_row+1))
 nsx_code_match_row_index = 1
