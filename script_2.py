f34c4f560869cda642a1997fe8f5e3e973d1d3c8
nguyendinhthong <nguyendinhthongit@gmail.com>
2020-04-01 15:23:50 +0700

    More explicit code, only add first match to item, more debug info to view the log

--- script_2.py
+++ script_2.py
@@ -10,7 +10,6 @@ excel_path_suffix = "./../thong_dmvt_lo1048/file_goc/"
 file = excel_path_suffix + "PL02, PL03_VTLK (Xong theo Ebom 11 du kien)_main.xlsx"
 print("Opening excel file: " + str(file))
 new_bom_wb = openpyxl.load_workbook(file)
-#print(new_bom_wb.sheetnames)
 # Select a specific sheet to work
 new_bom_sheet = "VTLK Thau"
 ws = new_bom_wb[new_bom_sheet]
@@ -19,7 +18,8 @@ print("Working with excel sheet: " + str(ws))
 component_code_list = []
 # iterate every row in column variable
 i = 1
-# limit to the rows where Ma NSX1 are available
+# limit to the rows where Component code are available 
+# (we don't handle the first info rows in the excel sheet because it's kind of time-consuming for now)
 wb_first_row = 3
 wb_last_row = 169
 for row in ws.iter_rows("H"):
@@ -32,60 +32,58 @@ print("There're total of " + str(wb_last_row-wb_first_row+1) + " component to be
 # print the list contain all ma nxs to map to target file along with its row index in previous file to Danh muc vat tu LINH KIEN.xlsx
 
 file1 = excel_path_suffix + "Danh muc vat tu LINH KIEN.xlsx"
-component_code_match_row_index = 1
+component_code_scanned_row_index = 1
 print("Opening excel file: " + str(file1))
 wb1 = openpyxl.load_workbook(file1, read_only=True)
 ws1 = wb1.active
 print("Working with excel sheet: " + str(wb1.sheetnames[0]))
-#print(wb1.sheetnames) 
-#print(len(component_code_list))
 # search each item in component_code_list in every row of column C (Ky ma hieu) in `Danh muc vat tu LINH KIEN.xlsx`
 # counter the number of original component to be searched
 component_code_cnt = 0
 none1_cnt = 0
-
 list_has_foreign_name = []
-
-# detect
-match_in_loop_new_detect = 0
+# detect how many matches are there for 1 input after scanning all the row on target column
+componentcode_match_in_loop_cnt = 0
 match_indices = []
 for item in component_code_list:
+    print("------")
+    # reset the counter for next row scanning loop    
+    component_code_scanned_row_index = 1
     for row in ws1.iter_rows("C"):
         for cell in row:
             # if we detect any matching by component code
             if cell.value == item[1]:
-                match_in_loop_new_detect = match_in_loop_new_detect + 1
-                component_code_cnt = component_code_cnt+1
-                
-                
-                # print("STT in second file: "+ str(component_code_match_row_index))
-                match_indices.append(component_code_match_row_index)
-               # print(component_code_match_row_index)
-               # print("Foreign Name (Ma cu): " + str(ws1.cell(None, component_code_match_row_index, 4).value))
-                item.append(component_code_match_row_index)
-                item.append(ws1.cell(None, component_code_match_row_index, 4).value)
-                if ws1.cell(None, component_code_match_row_index, 4).value == None:
-                    print("STT in first file: " + str(item[0]))
-                    print("None value detected, we won't use this for the next code")
-                    print("The component in old BOM match current new BOM, but its Foreign Name is None")
-                    none1_cnt = none1_cnt+1
-                else:
-                    list_has_foreign_name.append(item)
-
-            component_code_match_row_index = component_code_match_row_index+1
+                componentcode_match_in_loop_cnt = componentcode_match_in_loop_cnt + 1
+                # print("STT in second file: "+ str(component_code_scanned_row_index))
+                match_indices.append(component_code_scanned_row_index)
+               # print(component_code_scanned_row_index)
+               # print("Foreign Name (Ma cu): " + str(ws1.cell(None, component_code_scanned_row_index, 4).value))
+                # only consider the first match
+                if componentcode_match_in_loop_cnt == 1:
+                    component_code_cnt = component_code_cnt+1
+                    item.append(component_code_scanned_row_index)
+                    item.append(ws1.cell(None, component_code_scanned_row_index, 4).value)
+                    if ws1.cell(None, component_code_scanned_row_index, 4).value == None:
+                        print("STT in first file: " + str(item[0]))
+                        print("None value detected, we won't use this for the next code")
+                        print("The component in old BOM match current new BOM, but its Foreign Name is None")
+                        none1_cnt = none1_cnt+1
+                    else:
+                        list_has_foreign_name.append(item)
+            component_code_scanned_row_index = component_code_scanned_row_index+1
     print("STT in first file: " + str(item[0]))
     print("Component code (Ma NXS1): " + str(item[1]))
-    if match_in_loop_new_detect != 0:
-        print("Detect " + str(match_in_loop_new_detect) + " matching in second file With these row corresponding indices: ")
-        #print(":")
+
+    if componentcode_match_in_loop_cnt != 0:
+        print("Detect " + str(componentcode_match_in_loop_cnt) + " matching in second file With these row corresponding indices: ")
         print(match_indices)
         match_indices = []
     else:
         print("Didn't detect any matching")
-    match_in_loop_new_detect = 0
-        # reset the counter for next loop    
-    component_code_match_row_index = 1
+        #print("It means that it doesn't have new sap b1 component code")
+    componentcode_match_in_loop_cnt = 0
 
+print("------")
 print("Number of items matched by component code and have Foreign Name: " + str(component_code_cnt-none1_cnt) + " / " + str(wb_last_row-wb_first_row+1))
 nsx_code_match_row_index = 1
 file2 = excel_path_suffix + "List of Items 31-03-2020.xlsx"
@@ -93,33 +91,61 @@ print("Opening excel file: " + str(file2))
 wb2 = openpyxl.load_workbook(file2, read_only=True)
 ws2 = wb2.active
 print("Working with excel sheet: "+str(wb2.sheetnames[0]))
-add_cnt = 0
+match_by_oldsapcode_cnt = 0
+old_sap_code_match_in_loop_cnt = 0
+process_item_cnt = 0
+match_by_oldsapcode_indices = []
 for item in list_has_foreign_name:
+    print("------")
+    process_item_cnt = process_item_cnt + 1
     for row in ws2.iter_rows("F"):
         for cell in row:
             if cell.value == item[3]:
-                add_cnt = add_cnt + 1
+                old_sap_code_match_in_loop_cnt = old_sap_code_match_in_loop_cnt + 1
+                
                 #print(item[0])
                 #print("Component code (Ma NXS1): " + str(item[1]))
                 # print(item[2])
                 # print("Foreign Name (Ma cu): " + str(item[3]))
                 # print(nsx_code_match_row_index)
                 # print("Item No. (Ma moi): " + str(ws2.cell(None, nsx_code_match_row_index, 2).value))
-                print("Process component: " + str(add_cnt) + " / " + str(len(list_has_foreign_name)))
-                if ws2.cell(None, nsx_code_match_row_index, 2).value == None:
-                    print("None value detected")
-                # STT in third excel
-                item.append(nsx_code_match_row_index)
-                item.append(ws2.cell(None, nsx_code_match_row_index, 2).value)
-                # Ma cu
-                ws.cell(row=item[0], column=4).value = item[3]
-                # Ma moi
-                ws.cell(row=item[0], column=5).value = ws2.cell(None, nsx_code_match_row_index, 2).value
-                #print("----")
+                #print("Process component: " + str(match_by_oldsapcode_cnt) + " / " + str(len(list_has_foreign_name)))
+                match_by_oldsapcode_indices.append(nsx_code_match_row_index)
+                # only consider the first match
+                if old_sap_code_match_in_loop_cnt == 1:
+                    match_by_oldsapcode_cnt = match_by_oldsapcode_cnt + 1
+                    if ws2.cell(None, nsx_code_match_row_index, 2).value == None:
+                        print("None value detected")
+                    # STT in third excel
+                    item.append(nsx_code_match_row_index)
+                    item.append(ws2.cell(None, nsx_code_match_row_index, 2).value)
+                    # Write Ma cu
+                    ws.cell(row=item[0], column=4).value = item[3]
+                    # Ma moi
+                    ws.cell(row=item[0], column=5).value = ws2.cell(None, nsx_code_match_row_index, 2).value
+                
             nsx_code_match_row_index = nsx_code_match_row_index+1
-    # reset the counter for next loop    
+    
+    print("Processed item: " + str(process_item_cnt) + " / " + str(len(list_has_foreign_name)))
+    print("STT in first file: " + str(item[0]))
+    print("Component code (Ma NXS1): " + str(item[1]))
+    print("STT in second file: " + str(item[2]))
+    print("Old SAP B1 code: " + str(item[3]))
+    if old_sap_code_match_in_loop_cnt != 0:
+        print("Detect " + str(old_sap_code_match_in_loop_cnt) + " matching in second file With these row with following indices: ")
+        print(match_by_oldsapcode_indices)
+        match_by_oldsapcode_indices = []
+        #print("Matching")
+        #print(old_sap_code_match_in_loop_cnt)
+    else:
+        print("Didn't detect old_sap_code_match_in_loop_cnt matching")
+        print("It means that it doesn't have new sap b1 component code")
+    old_sap_code_match_in_loop_cnt = 0
+    # reset the row counter for next matching loop    
     nsx_code_match_row_index = 1
-
-print(component_code_list)
-new_bom_wb.save(filename = excel_path_suffix+'sample_book2.xlsx')
+print("There are: " + str(len(list_has_foreign_name)-match_by_oldsapcode_cnt) + " component without new sap b1 code")
+#print(component_code_list)
+des_file = excel_path_suffix+'sample_book_new_file_2.xlsx'
+print("Saving the file to location: " + str(des_file))
+new_bom_wb.save(filename = des_file)
 print("Finish excel mapping automation tool")
