a36dc3dc03a9c8dc9e87a8c5f0d13618e8bd9e5f
thong nguyen <nguyendinhthongit@gmail.com>
2021-07-29 17:18:47 +0700

    refactor code name...

--- knan_script/change_ftdi_knan_name.py
+++ knan_script/change_ftdi_knan_name.py
@@ -15,7 +15,7 @@ class bcolors:
 
 #path = '/mnt/e/du lieu nuc knan '
 path = '/mnt/d/Dulieu_NUC_KNAN/HDD_1TB_29July21'
-files = os.listdir(path)
+
 extracted_ftdi = ""
 
 ftdi_length = 8
@@ -45,17 +45,18 @@ def check_and_change_nucfolder_name(do_change_flag):
 	global fullpath_src_folder
 	global extracted_ftdi
 	count = 1
-	for folder_name in files:
+	root_folder_ls_list = os.listdir(path)
+	for each_item_folder_name in root_folder_ls_list:
 		valid_ftdi_flag = 0
 		good_name_flag = 0
 		print_header("***STT: " + str(count))
 		count = count + 1
-		#print(folder_name)
-		fullpath_src_folder = path+'/'+folder_name
+		#print(each_item_folder_name)
+		fullpath_src_folder = path+'/'+each_item_folder_name
 		print("Full path: " + fullpath_src_folder)
-		# all files and folder in fullpath_src_folder
+		# all root_folder_ls_list and folder in fullpath_src_folder
 		src_folder_ls = os.listdir(fullpath_src_folder)
-		underscore_index_list = find(folder_name, "_")
+		underscore_index_list = find(each_item_folder_name, "_")
 		underscore_count = len(underscore_index_list)
 		for tb_file in src_folder_ls:
 			valid_ftdi_flag = get_full_ftdi_from_file_name(tb_file)
@@ -64,19 +65,19 @@ def check_and_change_nucfolder_name(do_change_flag):
 		# check if folder name is already good
 		if valid_ftdi_flag == 1:
 			if underscore_count == 0:
-				# continue the for loop for this folder_name
+				# continue the for loop for this each_item_folder_name
 				print_fail("Discard this for loop as no underscore found")
 				continue
 			if underscore_count == 1:
-				new_folder_name = folder_name[0:underscore_index_list[0]+1]+extracted_ftdi
-				if (len(folder_name[underscore_index_list[0]:]) == ftdi_length+1):
+				new_folder_name = each_item_folder_name[0:underscore_index_list[0]+1]+extracted_ftdi
+				if (len(each_item_folder_name[underscore_index_list[0]:]) == ftdi_length+1):
 					good_name_flag = 1
 			if underscore_count >= 2:
 				if (underscore_index_list[1]-underscore_index_list[0] == ftdi_length+1):
 					good_name_flag = 1
-				new_folder_name = folder_name[0:underscore_index_list[0]+1]+extracted_ftdi+folder_name[underscore_index_list[1]:]
+				new_folder_name = each_item_folder_name[0:underscore_index_list[0]+1]+extracted_ftdi+each_item_folder_name[underscore_index_list[1]:]
 			if good_name_flag==1:
-				print_ok("Discard this as the folder_name is already OK")
+				print_ok("Discard this as the each_item_folder_name is already OK")
 				continue
 			new_fullpath_folder_name = path+'/'+new_folder_name
 			print("fullpath_src_folder: "+ fullpath_src_folder + "; new_fullpath_folder_name: " + new_fullpath_folder_name)
@@ -92,7 +93,6 @@ def check_and_change_nucfolder_name(do_change_flag):
 				print("Folder name isn't change")
 		else:
 			print_fail("None valid FTDI file is found")
-
 
 # The file can be Log_FTDI.txt or matlab file
 def get_full_ftdi_from_file_name(file_name):
@@ -117,9 +117,9 @@ def get_file_size(_file_name):
 		file_size = os.path.getsize(file_fullpath)
 		print_debug("File size = " + str(file_size))
 		return file_size
+
 tepm_file_size = 157286400
 def check_file_name_and_size(_fname, _fsize):
-	print_debug("check_file_name_and_size")
 	if _fname.find(".bin") != -1:
 		underscore_index_list = find(_fname, "_")
 		if len(underscore_index_list)==2:
@@ -135,27 +135,56 @@ def check_file_name_and_size(_fname, _fsize):
 		print("Log file detected")
 		return 3
 
-temp_file_count = 0
-generated_file_count = 0
-log_file_count = 0
+ok_temp_file_count = 0
+ok_generated_file_count = 0
+ok_log_file_count = 0
 
-def get_ftdi_and_check_all_files(_folder_content_ls):
+def print_check_file_content_message():
+	global ok_temp_file_count
+	global ok_generated_file_count
+	global ok_log_file_count
+
+	temp_str = "ok_temp_file_count: " + str(ok_temp_file_count)+"/9"
+	if ok_temp_file_count == 9:
+		print_ok(temp_str)
+	else:
+		print_fail(temp_str)
+	temp_str = "ok_generated_file_count: " + str(ok_generated_file_count)+"/5"
+	if ok_generated_file_count == 5:
+		print_ok(temp_str)
+	else:
+		print_fail(temp_str)
+	temp_str ="ok_log_file_count: " + str(ok_log_file_count)+"/1"
+	if ok_log_file_count == 1:
+		print_ok(temp_str)
+	else:
+		print_fail(temp_str)
+
+
+each_item_folder_ls_list = []
+
+def get_ftdi_and_check_all_files():
+	global each_item_folder_ls_list
+	global ok_temp_file_count
+	global ok_generated_file_count
+	global ok_log_file_count
 	done_get_ftdi_flag = 0
-	global temp_file_count
-	global generated_file_count
-	global log_file_count
-	for tb_file in _folder_content_ls:
+	ok_temp_file_count = 0
+	ok_generated_file_count = 0
+	ok_log_file_count = 0
+	for tb_file in each_item_folder_ls_list:
 		file_type = -1
 		if done_get_ftdi_flag == 0 and get_full_ftdi_from_file_name(tb_file) == 1:
 			done_get_ftdi_flag = 1
 		if done_get_ftdi_flag == 1:
 			file_type = check_file_name_and_size(tb_file, get_file_size(tb_file))
 			if file_type==1:
-				temp_file_count = temp_file_count + 1
+				ok_temp_file_count = ok_temp_file_count + 1
 			elif file_type==2:
-				generated_file_count = generated_file_count + 1
-			#elif file_type==3:
-
+				ok_generated_file_count = ok_generated_file_count + 1
+			elif file_type==3:
+				ok_log_file_count = ok_log_file_count + 1
+	print_check_file_content_message()
 def check_file_count(file_count):
 	print("File count: " + str(file_count))
 	if file_count == 0:
@@ -165,21 +194,23 @@ def check_file_count(file_count):
 			print_ok("File count ok")
 		else:
 			print_fail("File count error")
-		
+
 def check_complete_nuc_folder():
 	global fullpath_src_folder
+	global each_item_folder_ls_list
 	count = 1
-	for folder_name in files:
+	root_folder_ls_list = os.listdir(path)
+	for each_item_folder_name in root_folder_ls_list:
 		print_header("***STT: " + str(count))
 		count = count + 1
-		fullpath_src_folder = path + '/' + folder_name
+		fullpath_src_folder = path + '/' + each_item_folder_name
 		print(fullpath_src_folder)
-		# all files and folder in fullpath_src_folder
-		src_folder_ls = os.listdir(fullpath_src_folder)
-		src_folder_ls_count = len(src_folder_ls)
-		check_file_count(src_folder_ls_count)
-		if src_folder_ls_count>0:
-			get_ftdi_and_check_all_files(src_folder_ls)
+		# all root_folder_ls_list and folder in fullpath_src_folder
+		each_item_folder_ls_list = os.listdir(fullpath_src_folder)
+		each_item_folder_file_count = len(each_item_folder_ls_list)
+		check_file_count(each_item_folder_file_count)
+		if each_item_folder_file_count>0:
+			get_ftdi_and_check_all_files()
 		print_header("--------------------------------------------------------------------------------------------")
 	
 def main():
