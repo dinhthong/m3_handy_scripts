2148cd129bbaa65bc9cf6a6747549942d4772c96
dinhthong <nguyendinhthongit@gmail.com>
2021-08-02 13:50:57 +0700

    rename folder, add knan_software dir button

--- knan_nuc_script/gui_nuc.py
+++ knan_nuc_script/gui_nuc.py
@@ -21,7 +21,7 @@ config.read(pg_textfile_name)
 if config.has_section('main') == False:
     config.add_section('main')
     config.set('main', 'complete_nuc_parentfolder', "")
-
+    config.set('main', 'software_folder', "")
 def submitact():
     user = Username.get()
     passw = password.get()
@@ -29,8 +29,8 @@ def submitact():
 
 root = Tk()
 
-def brow_folder_cb():
-    print("Refreshing table")
+def browse_folder_cb():
+    #print("Refreshing table")
     file_path_variable = search_for_file_path()
     print ("\nfile_path_variable = ", file_path_variable)
     if file_path_variable != "":
@@ -38,6 +38,17 @@ def brow_folder_cb():
         # write to config file
         # config.add_section('main')
         config.set('main', 'complete_nuc_parentfolder', file_path_variable)
+        status_q.set("Change folder contains NUC folders to: "+file_path_variable)
+def br_software_folder_cb():
+    #print("Refreshing table")
+    file_path_variable = search_for_file_path()
+    print ("\nfile_path_variable = ", file_path_variable)
+    if file_path_variable != "":
+        dir_data_q.set(file_path_variable)
+        # write to config file
+        # config.add_section('main')
+        config.set('main', 'software_folder', file_path_variable)
+        status_q.set("Change folder contains KNAN_Software to: "+file_path_variable)
 def search_for_file_path ():
     currdir = os.getcwd()
     tempdir = filedialog.askdirectory(parent=root, initialdir=currdir, title='Please select a directory')
@@ -70,17 +81,17 @@ password.place(x = 150, y = 50, width = 100)
 submitbtn = tk.Button(wrapper0, text ="Login", 
                       bg ='blue', command = submitact)
 submitbtn.place(x = 150, y = 70, width = 55)
- 
-# User action wrapper
-btn_browse_dir = tk.Button(wrapper2, text ="BROWSE_DIR", 
-                       bg ='blue', command = brow_folder_cb)
-btn_browse_dir.place(x = 20, y = 20, width = 75)
 
 def search_btn_cb():
     print("Search button clicked")
+
+# User action wrapper
+btn_browse_dir = tk.Button(wrapper2, text ="BROWSE_DIR", 
+                       bg ='blue', command = browse_folder_cb)
+btn_browse_dir.place(x = 20, y = 20, width = 75)
 # Search selection
 # Search box
-q=StringVar()
+q = StringVar()
 lbl = Label(wrapper2, text="Search")
 lbl.pack(side=tk.LEFT, padx=10)
 ent = Entry(wrapper2, textvariable=q)
@@ -99,12 +110,30 @@ ent2.pack(side=tk.LEFT, padx=6)
 ent2.place(x=100, y=15, height=20, width=350)
 dir_q.set(config.get('main', 'complete_nuc_parentfolder'))
 
+# data_folder selection
+btn_browse_softwarefolder_dir = tk.Button(wrapper2, text ="SOFT_DIR", 
+                       bg ='blue', command = br_software_folder_cb)
+btn_browse_softwarefolder_dir.place(x = 400, y = 50, width = 75)
+
+dir_data_q = StringVar()
+ent_data = Entry(wrapper2, textvariable=dir_data_q)
+ent_data.pack(side=tk.LEFT, padx=6)
+ent_data.place(x=500, y=55, height=20, width=350)
+dir_data_q.set(config.get('main', 'software_folder'))
+
+# working log on GUI
+status_q = StringVar()
+status_q.set("HELLO")
+lbl = Label(wrapper1, width=50, textvariable = status_q)
+lbl.pack(side=tk.LEFT, padx=15)
+
 # create new record wrapper
 # https://www.kite.com/python/answers/how-to-make-an-array-of-strings-in-python
 
 def btn_main_check_complete_nuc_folder():
     print(dir_q.get())
     main_check_complete_nuc_folder(dir_q.get())
+    status_q.set("Done btn_main_check_complete_nuc_folder!")
 
 def btn_main_check_and_change_nucfolder_name():
     print(dir_q.get())
@@ -113,6 +142,7 @@ def btn_main_check_and_change_nucfolder_name():
 def btn_remove_status_msg_from_nuc_folder_name():
     #print(dir_q.get())
     remove_status_msg_from_nuc_folder_name(dir_q.get())
+    status_q.set("Done btn_remove_status_msg_from_nuc_folder_name!")
 
 btn_change_name = tk.Button(wrapper3, text ="CHANGE_NAME", 
                        bg ='#ffb3fe', command = btn_main_check_and_change_nucfolder_name)
